427848d0ccf15b0b3b680aa9a8c669004b94fb44
Gabriel Rodrigues <garodriguesfs10@gmail.com>
2022-06-25 15:43:07 -0300

    começando mensageria com RabbitMQ

--- DevFreela.API/Extensions/ServiceCollectionExtensions.cs
+++ DevFreela.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.Core.Repositories;
 using DevFreela.Core.Services;
 using DevFreela.Infrastructure.Auth;
+using DevFreela.Infrastructure.MessageBus;
 using DevFreela.Infrastructure.Payments;
 using DevFreela.Infrastructure.Persistence.Repositories;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +18,7 @@ namespace DevFreela.API.Extensions
 
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IPaymentService, PaymentService>();
-            //services.AddScoped<IMessageBusService, MessageBusService>();
+            services.AddScoped<IMessageBusService, MessageBusService>();
 
             return services;
         }
---
+++ DevFreela.Infrastructure/MessageBus/MessageBusService.cs
@@ -0,0 +1,42 @@
+﻿using DevFreela.Core.Services;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Infrastructure.MessageBus
+{
+    public class MessageBusService : IMessageBusService
+    {
+        private readonly ConnectionFactory _factory;
+        public MessageBusService(IConfiguration configuration)
+        {
+            _factory = new ConnectionFactory { HostName = "localhost" };
+        }
+        public void Publish(string queue, byte[] message)
+        {
+            using (var connection = _factory.CreateConnection()) 
+            {
+                using (var channel = connection.CreateModel()) 
+                {
+                    //declarar e garantir que a fila esteja criada
+
+                    channel.QueueDeclare(
+                        queue: queue,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: queue,
+                        basicProperties: null,
+                        body: message);
+
+                }
+            }
+        }
+    }
+}
